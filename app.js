17a1b96284cb3b04ef08b3793b6357f18fea2473
Shashank Dubey <jfriday464@gmail.com>
2022-02-20 10:44:39 +0530

    Port fixes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-const Port = 5000 || process.env.PORT;
+const Port = process.env.PORT || 5000;
 var cors = require("cors");
 
 app.use(cors());
@@ -15,30 +15,30 @@ app.get("/api", (req, res) => {
       .json({ error: false, message: "Server is up and running!" });
 });
 
-// io.on("connection", (socket) => {
-//    console.log("a user connected");
-//    socket.on("disconnect", () => {
-//       console.log("user disconnected");
-//    });
+io.on("connection", (socket) => {
+   console.log("a user connected");
+   socket.on("disconnect", () => {
+      console.log("user disconnected");
+   });
 
-//    socket.on("chat message", (msg) => {
-//       console.log("message: " + msg);
-//       io.emit("chat message", msg);
-//    });
-// });
+   socket.on("chat message", (msg) => {
+      console.log("message: " + msg);
+      io.emit("chat message", msg);
+   });
+});
 
 app.get("/", (req, res) => {
    try {
-      return res
-         .status(200)
-         .json({ error: false, message: "Server is up and running!" });
-      // return res.sendFile(__dirname + "/client/index.html");
+      // return res
+      //    .status(200)
+      //    .json({ error: false, message: "Server is up and running!" });
+      return res.sendFile(__dirname + "/client/index.html");
    } catch (error) {
       console.log(error);
       throw error;
    }
 });
 
-app.listen(Port, () => {
+server.listen(Port, () => {
    console.log("listening on *:", Port);
 });
