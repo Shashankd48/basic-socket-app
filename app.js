46927acbead0f553b656edd25300be6b85249ede
Shashank Dubey <jfriday464@gmail.com>
2022-02-20 10:10:09 +0530

    Cors enabled

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 const Port = 5000 || process.env.PORT;
+var cors = require("cors");
+
+app.use(cors());
 
 app.get("/api", (req, res) => {
    return res
